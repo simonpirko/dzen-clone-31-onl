45927d9038deb27a628d3e91822b05be8ff28931
Simon Pirko <sipi7gm@gmail.com>
2025-03-13 19:06:42 +0300

    Initial commit

---
+++ src/main/java/by/tms/dzenclone31onl/DzenClone31OnlApplication.java
@@ -0,0 +1,13 @@
+package by.tms.dzenclone31onl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DzenClone31OnlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DzenClone31OnlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/tms/dzenclone31onl/DzenClone31OnlApplicationTests.java
@@ -0,0 +1,13 @@
+package by.tms.dzenclone31onl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DzenClone31OnlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
