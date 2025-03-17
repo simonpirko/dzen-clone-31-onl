93326678f97a40dffb5386f83d92c2cb72b9e810
PolyaSidorenko <polina11204@gmail.com>
2025-03-18 00:34:01 +0300

    Создание регистрации и входа

--- src/main/java/by/tms/dzenclone31onl/domain/Recommendation.java
+++ src/main/java/by/tms/dzenclone31onl/domain/Recommendation.java
@@ -14,9 +14,11 @@ public class Recommendation {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
+
     @ManyToOne
     @JoinColumn(name = "article_id", nullable = false)
     private Article article;
